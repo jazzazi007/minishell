0af3f654803d1897bb2874f603f5ed3c9b60efd1
Rama Albliwi <ralbliwi@c1r6s4.42amman.com>
2025-05-10 17:35:04 +0300

    signals DONE

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ralbliwi <ralbliwi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:00:44 by ralbliwi          #+#    #+#             */
-/*   Updated: 2025/05/07 18:26:54 by ralbliwi         ###   ########.fr       */
+/*   Updated: 2025/05/10 17:24:19 by ralbliwi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,14 @@ int	main(void)
 {
 	char	*input;
 	setup_signal_handlers();
-	
 	while (1)
 	{
-		// setup_signal_handlers();
-	
 		input = readline("\001\033[31m\002minishell 🔥 > \001\033[0m\002");
 		if (!input)
 		{
 			printf("exit\n");
 			break ;
 		}
-		// if (g_exit_status == 130)
-		// {
-		// 	// printf("fjjf\n");
-		// 	g_exit_status = 0;
-		// 	// exit (130);
-		// }
 		if (*input)
 			add_history(input);
 		if (ft_strcmp(input, "exit") == 0)
--- signal.c
+++ signal.c
@@ -6,35 +6,14 @@
 /*   By: ralbliwi <ralbliwi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:00:28 by ralbliwi          #+#    #+#             */
-/*   Updated: 2025/05/07 18:07:03 by ralbliwi         ###   ########.fr       */
+/*   Updated: 2025/05/10 17:24:51 by ralbliwi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int g_exit_status = 0;
-//ctrl+c
-// void sigint_handler(int signum)
-// {
-//     (void)signum;
-//     write(1,"\n",1);
-//     rl_replace_line("",0);
-//     rl_on_new_line();
-//     rl_redisplay();
-//     g_exit_status = 130;
-// }
-
-// void setup_signal_handlers(void)
-// {
-//     struct sigaction sa;
-
-//     sa.sa_handler = sigint_handler;  // set handler
-//     sa.sa_flags = SA_RESTART;        // automatically restart interrupted syscalls
-//     sigemptyset(&sa.sa_mask);        // clear mask so no signals are blocked
-
-//     sigaction(SIGINT, &sa, NULL);
-// }
-
+int g_exit_status = 0; // Global variable to track exit status
+// Handler for Ctrl+C (SIGINT)
 void sigint_handler(int signum)
 {
     (void)signum;
@@ -44,16 +23,20 @@ void sigint_handler(int signum)
     rl_redisplay();
     g_exit_status = 130;
 }
-
+// Setup handlers for SIGINT and SIGQUIT
 void setup_signal_handlers(void)
 {
     struct sigaction sa;
 
+    // Set handler for SIGINT (Ctrl+C)
     sa.sa_handler = sigint_handler;
     sa.sa_flags = SA_RESTART;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGINT, &sa, NULL);
 
+    // Ignore SIGQUIT (Ctrl-\)
     sa.sa_handler = SIG_IGN;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
     sigaction(SIGQUIT, &sa, NULL);
 }
